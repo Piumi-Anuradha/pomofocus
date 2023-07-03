af1f798276ef41bd5389a6b55391035e461576e1
Piumi-Anuradha <piumianuradha26@gmail.com>
2023-07-03 06:51:11 +0000

    link js file in index.html, write in the js file

---
+++ script.js
@@ -0,0 +1,13 @@
+const startEl = document.getElementById("start");
+const stopEl = document.getElementById("stop");
+const resetEl = document.getElementById("reset");
+const timerEl = document.getElementById("timer");
+
+function startTimer(){
+  console.log("start");
+}
+
+
+
+
+startEl.addEventListener("click",startTimer)
