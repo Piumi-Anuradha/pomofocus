52be3f7338d2f74c5fca623ff51e7a29830399e0
Piumi-Anuradha <piumianuradha26@gmail.com>
2023-07-03 07:19:41 +0000

    update timer

--- script.js
+++ script.js
@@ -3,11 +3,31 @@ const stopEl = document.getElementById("stop");
 const resetEl = document.getElementById("reset");
 const timerEl = document.getElementById("timer");
 
+
+let interval 
+let timeLeft = 1500;
+
+function updateTimer(){
+  let minutes = Math.floor(timeLeft/60)
+  let seconds = timeLeft % 60
+}
+
 function startTimer(){
-  console.log("start");
+  interval = setInterval(()=>{
+    
+  },1000)
+}
+
+function stopTimer(){
+  console.log("stop");
+}
+
+function resetTimer(){
+  console.log("reset");
 }
 
 
 
-
 startEl.addEventListener("click",startTimer)
+stopEl.addEventListener("click",stopTimer)
+resetEl.addEventListener("click",resetTimer)
